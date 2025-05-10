850713c0aeb9f1acc2c0c682f8c6726dc6c10950
Imad-Dabagh <imaddabagh@gmail.com>
2025-05-10 23:48:19 +0100

    Completed Login Page

--- frontend/notes_app/tailwind.config.js
+++ frontend/notes_app/tailwind.config.js
@@ -1,7 +1,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: ["bg-primary", "text-primary"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#2B85FF",
+        secondary: "#EF863E",
+      },
+    },
   },
   plugins: [],
 };
